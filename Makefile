1ab6605e9c733830c8f70f2b0b9b94f3e9030527
Alex Leidwein <leiti34@gmail.com>
2016-12-22 15:14:21 +0100

    Added comments

--- Makefile
+++ Makefile
@@ -1,13 +1,15 @@
-#Autor: Alex Leidwein
+# Autor: Alex Leidwein
+
 CC = /usr/bin/gcc
 SRC = main.c
 BIN = $(SRC:%.c=%.out)
 
 all: main
-		
+
 main: main.c
-	gcc main.c -o main.out -lpthread 
+	gcc main.c -o main.out -lpthread
 
 .PHONY: clean
+
 clean:
 	rm -rf $(BIN)
--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 /************************************/
 /* Author: Alex Leidwein            */
 /* Name: Cigarette Smokers Problem  */
-/* Revision: 1.0                    */
-/* Date: 01.12.2016                 */
+/* Revision: 1.1                    */
+/* Date: 22.12.2016                 */
 /************************************/
 
 /** Includes */
@@ -20,15 +20,16 @@
 sem_t sTobacco, sPaper, sMatch;
 sem_t sAgent;
 
-/* Argument ID:
- * wait for:
+/*
+ * Smoker waits for the missing Item
+ * Argument wait for:
  * - 0 ... Tobacco
  * - 1 ... Paper
  * - 2 ... Match
  */
 void *smoker(void * arg)
 {
-    int id = *(int*)arg;
+    int id = *(int*)arg; // Convert argument to int
 
     for(;;)
     {
@@ -51,26 +52,26 @@ void *smoker(void * arg)
                 printf("%d got Match\n", id);
                 sem_post(&sAgent);
                 break;
-                
+
             default: // Error case
-                printf("ERROR: Invalid argument ID: %d\n", id);
+                perror("ERROR: Invalid argument ID: %d\n", id);
                 break;
         }
     }
 }
 
+/*
+ * Agent sets a random Item
+ */
 void *agent(void * arg)
 {
     for(;;)
     {
-        /** Wait */
-        sem_wait(&sAgent);
+        sem_wait(&sAgent);  // Wait for smoker
 
-        /** Delay for better visability */
-        usleep(1000000);
+        usleep(1000000);  // Delay for better visability
 
-        /** Generate a random ID for a random item */
-        int id = rand() % 3;
+        int id = rand() % 3;  // Generate a random ID for a random item
 
         switch(id)
         {
@@ -88,7 +89,7 @@ void *agent(void * arg)
                 puts("Agent set Match");
                 sem_post(&sMatch);
                 break;
-                
+
             default: // Error case
                 printf("ERROR wrong Agent ID: %d\n", id);
                 break;
@@ -96,20 +97,23 @@ void *agent(void * arg)
     }
 }
 
+/*
+ * Main
+ */
 int main(int argc, char* argv[])
 {
-    /** Init Semaphores */
+    /** Init semaphores */
     sem_init(&sTobacco, 0, 0);
     sem_init(&sPaper, 0, 0);
     sem_init(&sMatch, 0, 0);
-    
+
     sem_init(&sAgent, 0, 1);
 
-    /** Threads erstellen */
+    /** Define threads */
     pthread_t tSmokers[SMOKERS], tAgent;
 
     int smokersID[SMOKERS];
-    
+
     /** Create smokers */
     for(int i = 0; i < SMOKERS; i++)
     {
@@ -117,13 +121,13 @@ int main(int argc, char* argv[])
         pthread_create(&tSmokers[i], NULL, smoker, &smokersID[i]);
     }
 
-    /** Craete Agent */
+    /** Craete agent */
     pthread_create(&tAgent, NULL, agent, NULL);
 
     /** Wait for smokers */
     for (int i = 0; i < SMOKERS; i++)
         pthread_join(tSmokers[i], NULL);
-    
+
     /** Wait for agent */
     pthread_join(tAgent, NULL);
 
